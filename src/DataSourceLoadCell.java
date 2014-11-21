fb5571c4bb3976bdf5ea6d28ba4e685107c1af47
James Jarvis <jj@aprsworld.com>
2014-11-21 08:16:16 -0600

    added GUI

--- src/DataSourceLoadCell.java
+++ src/DataSourceLoadCell.java
@@ -1,6 +1,10 @@
+import java.awt.Dimension;
+import java.awt.Font;
 import java.io.IOException;
 
 import javax.comm.SerialPort;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class DataSourceLoadCell implements ListenerLoadCell {
@@ -8,6 +12,10 @@ public class DataSourceLoadCell implements ListenerLoadCell {
 	
 	public String outPrefix;
 
+	/* GUI stuff */
+	public static final boolean gui=true;
+	protected JLabel labelCurrentValue;
+	
 	/* fire it off via TCP/IP */
 	public void packetReceivedTemperature(int[] rawBuffer) {
 		StringBuilder sb=new StringBuilder();
@@ -22,6 +30,27 @@ public class DataSourceLoadCell implements ListenerLoadCell {
 		
 		//System.out.println("# We received (and trimmed) -> '" + sb.toString() + "'");
 		System.out.println(outPrefix + sb.toString());
+		
+		
+		if ( gui ) {
+			labelCurrentValue.setText(sb.toString() + " pounds");
+		}
+	}
+	
+	protected void setupGUI() {
+		JFrame frame = new JFrame("DataSourceLoadCell");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Add the ubiquitous "Hello World" label.
+		labelCurrentValue = new JLabel("Waiting for data...");
+		labelCurrentValue.setFont(new Font("Serif", Font.PLAIN, 64));
+		frame.getContentPane().add(labelCurrentValue);
+
+		//Display the window.
+		frame.setMinimumSize(new Dimension(400,150));
+		frame.pack();
+		
+		frame.setVisible(true);
 	}
 
 	public void run(String[] args) throws IOException {
@@ -42,12 +71,17 @@ public class DataSourceLoadCell implements ListenerLoadCell {
 		System.err.println("# prefixing data with '" + prefix + "' before sending to DataGS");
 		SerialReaderLoadstar ser810W = new SerialReaderLoadstar(serialPortName, 230400, SerialPort.PARITY_NONE);
 
+		if ( gui ) {
+			setupGUI();
+		}
+
+		
 		ser810W.addPacketListener(this);
 
 	}
 
 	public static void main(String[] args) throws IOException {
-		System.err.println("# Major version: 2014-11-07 (precision)");
+		System.err.println("# Major version: 2014-11-21 (precision)");
 		System.err.println("# java.library.path: " + System.getProperty( "java.library.path" ));
 
 		DataSourceLoadCell d = new DataSourceLoadCell();
